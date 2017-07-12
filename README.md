3f353a8a7c603e8a3263c755e902e42812291ecf
Tommy Beadle <tbeadle@gmail.com>
2017-07-12 10:03:53 -0400

    Initial version of drf_link_header_pagination.

---
+++ README.md
@@ -0,0 +1,90 @@
+# djangorestframework-link-header-pagination
+
+![build-status-image][build-status-image] ![pypi-version][pypi-version]
+
+## Overview
+
+Provide pagination using a `Link` HTTP header as described in [Github's developer documentation](github-pagination)
+
+This pagination style accepts a single page number in the request query parameters. The response uses an HTTP header called `Link` to provide the URLs for the next, previous, first, and last pages. If you are using Python's [Requests][requests] library to make the request, this header is automatically parsed for you as described [here][requests-link-header].
+
+**Request**:
+
+    GET https://api.example.org/accounts/?page=4
+
+**Response**:
+
+    HTTP 200 OK
+    Link: <https://api.example.org/accounts/>; rel="first", <https://api.example.org/accounts/?page=3>; rel="prev", <https://api.example.org/accounts/?page=5>; rel="next", <https://api.example.org/accounts/?page=11>; rel="last"
+
+    [
+       {
+           "id": 1,
+           "name": "item one",
+       },
+       ...
+    ]
+
+## Requirements
+
+ -  Python (2.7, 3.3, 3.4, 3.5, 3.6)
+ -  Django (1.8, 1.10, 1.11)
+ -  Django REST Framework (3.6)
+
+## Installation
+
+Install using ``pip``:
+
+```bash
+$ pip install djangorestframework-link-header-pagination
+```
+
+## Setup
+
+Add `drf_link_header_pagination` to your project's `INSTALLED_APPS` setting.
+
+To enable the `LinkHeaderPagination` style globally, use the following configuration, modifying the `PAGE_SIZE` as desired:
+
+```python
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'drf_link_header_pagination.LinkHeaderPagination',
+    'PAGE_SIZE': 100
+}
+```
+
+On `GenericAPIView` subclasses you may also set the `pagination_class` attribute to select `LinkHeaderPagination` on a per-view basis.
+
+## Configuration
+
+The configuration is the same as for
+[`PageNumberPagination`](page-number-pagination-configuration).
+
+## Testing
+
+Install testing requirements.
+
+```bash
+$ pip install -r requirements.txt
+```
+
+Run with runtests.
+
+```bash
+$ ./runtests.py
+```
+
+You can also use the excellent [tox](tox) testing tool to run the tests
+against all supported versions of Python and Django. Install tox
+globally, and then simply run:
+
+```bash
+$ tox
+```
+
+[build-status-image]: https://secure.travis-ci.org/tbeadle/django-rest-framework-link-header-pagination.svg?branch=master
+[pypi-version]: https://img.shields.io/pypi/v/djangorestframework-link-header-pagination.svg
+[github-pagination]: https://developer.github.com/guides/traversing-with-pagination/
+[requests]: http://docs.python-requests.org
+[requests-link-header]: http://docs.python-requests.org/en/master/user/advanced/#link-headers
+[page-number-pagination-configuration]: http://www.django-rest-framework.org/api-guide/pagination/#pagenumberpagination
+[tox]: http://tox.readthedocs.org/en/latest/
---
+++ tests/test_link_header_pagination.py
@@ -0,0 +1,112 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+import pytest
+from rest_framework import exceptions
+from rest_framework.pagination import PAGE_BREAK, PageLink
+from rest_framework.request import Request
+from rest_framework.test import APIRequestFactory
+
+import drf_link_header_pagination
+
+factory = APIRequestFactory()
+
+
+class TestLinkHeaderPagination:
+    """
+    Unit tests for `pagination.LinkHeaderPagination`.
+    """
+
+    def setup(self):
+        class ExamplePagination(drf_link_header_pagination.LinkHeaderPagination):
+            page_size = 5
+
+        self.pagination = ExamplePagination()
+        self.queryset = range(1, 101)
+
+    def paginate_queryset(self, request):
+        return list(self.pagination.paginate_queryset(self.queryset, request))
+
+    def get_paginated_response(self, queryset):
+        return self.pagination.get_paginated_response(queryset)
+
+    def get_html_context(self):
+        return self.pagination.get_html_context()
+
+    def test_no_page_number(self):
+        request = Request(factory.get('/'))
+        queryset = self.paginate_queryset(request)
+        response = self.get_paginated_response(queryset)
+        context = self.get_html_context()
+        assert queryset == [1, 2, 3, 4, 5]
+        assert response.data == [1, 2, 3, 4, 5]
+        assert response['Link'] == (
+            '<http://testserver/?page=2>; rel="next", '
+            '<http://testserver/?page=20>; rel="last"'
+        )
+        assert context == {
+            'previous_url': None,
+            'next_url': 'http://testserver/?page=2',
+            'page_links': [
+                PageLink('http://testserver/', 1, True, False),
+                PageLink('http://testserver/?page=2', 2, False, False),
+                PageLink('http://testserver/?page=3', 3, False, False),
+                PAGE_BREAK,
+                PageLink('http://testserver/?page=20', 20, False, False),
+            ]
+        }
+        assert self.pagination.display_page_controls
+        assert isinstance(self.pagination.to_html(), type(''))
+
+    def test_second_page(self):
+        request = Request(factory.get('/', {'page': 2}))
+        queryset = self.paginate_queryset(request)
+        response = self.get_paginated_response(queryset)
+        context = self.get_html_context()
+        assert queryset == [6, 7, 8, 9, 10]
+        assert response.data == [6, 7, 8, 9, 10]
+        assert response['Link'] == (
+            '<http://testserver/>; rel="first", '
+            '<http://testserver/>; rel="prev", '
+            '<http://testserver/?page=3>; rel="next", '
+            '<http://testserver/?page=20>; rel="last"'
+        )
+        assert context == {
+            'previous_url': 'http://testserver/',
+            'next_url': 'http://testserver/?page=3',
+            'page_links': [
+                PageLink('http://testserver/', 1, False, False),
+                PageLink('http://testserver/?page=2', 2, True, False),
+                PageLink('http://testserver/?page=3', 3, False, False),
+                PAGE_BREAK,
+                PageLink('http://testserver/?page=20', 20, False, False),
+            ]
+        }
+
+    def test_last_page(self):
+        request = Request(factory.get('/', {'page': 'last'}))
+        queryset = self.paginate_queryset(request)
+        response = self.get_paginated_response(queryset)
+        context = self.get_html_context()
+        assert queryset == [96, 97, 98, 99, 100]
+        assert response.data == [96, 97, 98, 99, 100]
+        assert response['Link'] == (
+            '<http://testserver/>; rel="first", '
+            '<http://testserver/?page=19>; rel="prev"'
+        )
+        assert context == {
+            'previous_url': 'http://testserver/?page=19',
+            'next_url': None,
+            'page_links': [
+                PageLink('http://testserver/', 1, False, False),
+                PAGE_BREAK,
+                PageLink('http://testserver/?page=18', 18, False, False),
+                PageLink('http://testserver/?page=19', 19, False, False),
+                PageLink('http://testserver/?page=20', 20, True, False),
+            ]
+        }
+
+    def test_invalid_page(self):
+        request = Request(factory.get('/', {'page': 'invalid'}))
+        with pytest.raises(exceptions.NotFound):
+            self.paginate_queryset(request)
