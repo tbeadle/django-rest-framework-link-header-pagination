979857a1404808a414682da059ea6890e60b27a6
Shannon I'Ons <shannnon@dataprophet.com>
2020-01-17 15:27:22 +0200

    feat: Cursor based pagination support added. Header link and response link combination support added.

--- drf_link_header_pagination/__init__.py
+++ drf_link_header_pagination/__init__.py
@@ -1,38 +1,46 @@
-from rest_framework.pagination import PageNumberPagination
+from collections import OrderedDict
+from rest_framework.pagination import PageNumberPagination, CursorPagination
 from rest_framework.response import Response
 from rest_framework.utils.urls import remove_query_param, replace_query_param
 
-__version__ = '0.1.1'
+__version__ = "0.1.2"
 
 __all__ = [
-    'LinkHeaderPagination',
+    "LinkHeaderPagination",
+    "LinkHeaderCursorPagination",
+    "LinkHeaderJsonKeyCursorPagination",
 ]
 
 
-class LinkHeaderPagination(PageNumberPagination):
+class LinkHeaderMixin:
+    def get_headers(self):
+        """Prepare and return link headers."""
+        links = ((self.get_previous_link(), "prev"), (self.get_next_link(), "next"))
+
+        if isinstance(self, PageNumberPagination):
+            links = (
+                *links,
+                (self.get_first_link(), "first"),
+                (self.get_last_link(), "last"),
+            )
+
+        header_links = []
+        for url, label in links:
+            if url is not None:
+                header_links.append('<{}>; rel="{}"'.format(url, label))
+
+        headers = {"Link": ", ".join(header_links)} if header_links else {}
+        return headers
+
+
+class LinkHeaderPagination(PageNumberPagination, LinkHeaderMixin):
     """ Inform the user of pagination links via response headers, similar to
     what's described in
     https://developer.github.com/guides/traversing-with-pagination/.
     """
+
     def get_paginated_response(self, data):
-        next_url = self.get_next_link()
-        previous_url = self.get_previous_link()
-        first_url = self.get_first_link()
-        last_url = self.get_last_link()
-
-        links = []
-        for url, label in (
-            (first_url, 'first'),
-            (previous_url, 'prev'),
-            (next_url, 'next'),
-            (last_url, 'last'),
-        ):
-            if url is not None:
-                links.append('<{}>; rel="{}"'.format(url, label))
-
-        headers = {'Link': ', '.join(links)} if links else {}
-
-        return Response(data, headers=headers)
+        return Response(data, headers=self.get_headers())
 
     def get_first_link(self):
         if not self.page.has_previous():
@@ -47,7 +55,33 @@ class LinkHeaderPagination(PageNumberPagination):
         else:
             url = self.request.build_absolute_uri()
             return replace_query_param(
-                url,
-                self.page_query_param,
-                self.page.paginator.num_pages,
+                url, self.page_query_param, self.page.paginator.num_pages
             )
+
+
+class LinkHeaderCursorPagination(CursorPagination, LinkHeaderMixin):
+    """
+    Customized cursor pagination class utilizing Link header.
+    Provides standard json key pagination links in addition to LinkHeader.
+    """
+
+    def get_paginated_response(self, data):
+        return Response(data, headers=self.get_headers())
+
+
+class LinkHeaderJsonKeyCursorPagination(CursorPagination, LinkHeaderMixin):
+    """
+    Provides cursor pagination with standard json key pagination links
+    in addition to LinkHeader.
+    """
+
+    def get_paginated_response(self, data):
+        next_url = self.get_next_link()
+        previous_url = self.get_previous_link()
+
+        return Response(
+            OrderedDict(
+                [("next", next_url), ("previous", previous_url), ("results", data)]
+            ),
+            headers=self.get_headers(),
+        )
